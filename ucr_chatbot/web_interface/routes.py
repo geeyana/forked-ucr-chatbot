598ade9b665c60425dc9393d8e83f5666e9eb9b6
shreyas-reddy18 <snallapa1@gmail.com>
2025-07-15 16:21:28 -0700

    Fixed typing problems

--- ucr_chatbot/web_interface/routes.py
+++ ucr_chatbot/web_interface/routes.py
@@ -1,30 +1,39 @@
 from flask import (
     Blueprint,
     render_template,
-    url_for,
-    redirect,
 )
-from ucr_chatbot.db.models import *
-from sqlalchemy import select, insert
+from ucr_chatbot.db.models import Session, engine, Courses, ParticipatesIn
+from sqlalchemy import select
 
 
-bp = Blueprint("routes", __name__)
+bp = Blueprint("web_routes", __name__)
 
-user_email = 'test@ucr.edu'
+user_email = "test@ucr.edu"
+
 
 @bp.route("/")
 def course_selection():
+    """Renders the main landing page with a list of the user's courses.
+
+    This route queries the database to find all courses the current user
+    is enrolled in and passes that list to the landing_page.html
+    template for display.
+
+    :return: The rendered HTML content of the landing page.
+    """
     print("web_interface")
     with Session(engine) as session:
-        stmt = (select(Courses).join(ParticipatesIn, Courses.id == ParticipatesIn.course_id).where(ParticipatesIn.email ==  user_email))
-        result = session.execute(stmt)
+        stmt = (
+            select(Courses)
+            .join(ParticipatesIn, Courses.id == ParticipatesIn.course_id)
+            .where(ParticipatesIn.email == user_email)
+        )
+        courses = session.execute(stmt).scalars().all()
 
-        courses = []
-        for row in result:
-            courses.append(row[0])
-
-    return render_template("landing_page.html",courses=courses,)
-
+    return render_template(
+        "landing_page.html",
+        courses=courses,
+    )
 
 
 @bp.route("/new_conversation/<int:course_id>/chat")
@@ -32,7 +41,7 @@ def new_conversation(course_id: int):
     """Redirects to a page with a new conversation for a course.
     :param course_id: The id of the course for which a conversation will be initialized.
     """
-  
+
     return render_template("conversation.html")
 
 
